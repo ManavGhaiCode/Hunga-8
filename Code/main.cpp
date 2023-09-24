4eecd933bfd08e022d94eb33ce31f67def2f841f
ManavGhaiCode <manavghai12october@gmail.com>
2023-09-24 11:23:41 +0530

    redo main.cpp

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,5 +1,15 @@
+/*
+
+* This is an emulator so things might not match up if you build this in Hardware.
+* Have fun looking at my code. Kindly review my code, If you have the time!
+
+*/
+
 #include <iostream>
+#include "../Common/Instructions.h"
 
 int main() {
+    std::cout << "Welcome to the Hunga-8!" << std::endl;
+
     return 0;
 }
